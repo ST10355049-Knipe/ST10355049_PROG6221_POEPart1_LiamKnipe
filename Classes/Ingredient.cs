cb0df1bc5042ac9704a9f811708254d3857410ee
Liam Knipe <129791138+ST10355049-Knipe@users.noreply.github.com>
2024-04-14 16:06:03 +0200

    building block
    This class serves as a building block for defining ingredients used in recipes. The constructor allows me to create a Ingredient object which allows me to structurally represent the ingredients inside my recipe management system.

---
+++ Classes/Ingredient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10355049_PROG6221_POEPart1_LiamKnipe.Classes
+{
+    internal class Ingredient
+    {
+        public string Name { get; }
+        public double Quantity { get; }
+        public string UnitOfMeasurement { get; }
+      
+        public Ingredient(string name, double quantity, string unitOfMeasurement)
+        {
+            Name = name;
+            Quantity = quantity;
+            UnitOfMeasurement = unitOfMeasurement;
+        }
+    }
+}
